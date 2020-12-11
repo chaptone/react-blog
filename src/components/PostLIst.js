532388fc1e14096b6d46e37346339c8569df8b66
chaptone <rakkan.j@gmail.com>
2020-12-12 00:35:56 +0700

    Add redux-thunk

---
+++ src/components/PostLIst.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+const PostList = ({ fetchPosts }) => {
+    useEffect(() => {
+        fetchPosts();
+    });
+    return <div>Post List</div>;
+};
+
+export default connect(null, { fetchPosts })(PostList);
